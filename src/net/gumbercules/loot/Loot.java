8022716df79d6164b89be138e1f5e0bdd309a227
Christopher McCurdy <christopher.mccurdy@gmail.com>
2008-10-06 16:29:51 +0000

    added xml gui designs

---
+++ src/net/gumbercules/loot/Loot.java
@@ -0,0 +1,13 @@
+package net.gumbercules.loot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Loot extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.options);
+    }
+}
