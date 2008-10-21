fe3dca4b16635819aba66ddb3a4badae781f9fa3
Christopher McCurdy <christopher.mccurdy@gmail.com>
2008-10-21 19:20:27 +0000

    added code to write and calculateDueDate methods

--- src/net/gumbercules/loot/RepeatSchedule.java
+++ src/net/gumbercules/loot/RepeatSchedule.java
@@ -1,12 +1,13 @@
 package net.gumbercules.loot;
 
-import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import android.database.*;
 import android.database.sqlite.*;
 
 public class RepeatSchedule
+implements Cloneable
 {
 	// repetition iterator type
 	public static final int NO_REPEAT	= 0;
@@ -34,28 +35,183 @@ public class RepeatSchedule
 	int custom;			// used only for custom types
 	Date start;			// start date
 	Date end;			// end date
-	Date due;			// date of the next repetition
-	int id;				// id of the database repeat_pattern, if available
+	private Date due;	// date of the next repetition
+	private int id;		// id of the database repeat_pattern, if available
 	
 	public RepeatSchedule()
 	{
 		this.id = -1;
+		this.due = null;
+		this.start = null;
+		this.end = null;
+		this.iter = -1;
+		this.freq = -1;
+		this.custom = -1;
 	}
 	
-	public RepeatSchedule( int it, int fr, int cu, Date st, Date en, Date du )
+	public RepeatSchedule( int it, int fr, int cu, Date st, Date en )
 	{
 		this.id = -1;
+		this.due = null;
 		this.iter = it;
 		this.freq = fr;
 		this.custom = cu;
 		this.start = st;
 		this.end = en;
-		this.due = du;
 	}
 	
-	public int write()
+	protected Object clone()
+	throws CloneNotSupportedException
 	{
-		return -1;
+		return (RepeatSchedule)super.clone();
+	}
+	
+	public int write(int trans_id)
+	{
+		this.due = this.calculateDueDate();
+		if (this.due == null || this.start == null)
+			return -1;
+		
+		if (this.id == -1)
+			return newRepeat(trans_id);
+		else
+			return updateRepeat(trans_id);
+	}
+	
+	private int newRepeat(int trans_id)
+	{
+		long start_time = this.start.getTime(), end_time = 0;
+		try
+		{
+			end_time = this.end.getTime();
+		}
+		catch (Exception e) { }
+		
+		String insert = "insert into repeat_pattern (start_date,end_date,iterator,frequency," +
+						"custom,due values (" + start_time + "," + end_time + "," + this.iter +
+						"," + this.freq + "," + this.custom + "," + this.due.getTime() + ")";
+		
+		SQLiteDatabase lootDB = Database.getDatabase();
+		lootDB.beginTransaction();
+		
+		try
+		{
+			lootDB.execSQL(insert);
+		}
+		catch (SQLException e)
+		{
+			lootDB.endTransaction();
+			return -1;
+		}
+		
+		String[] columns = {"max(id)"};
+		Cursor cur = lootDB.query("repeat_pattern", columns, null, null, null, null, null);
+		this.id = cur.getInt(0);
+		cur.close();
+		
+		if (!this.writeTransactionToRepeatTable(trans_id))
+		{
+			lootDB.endTransaction();
+			return -1;
+		}
+		
+		Transaction trans = Transaction.getTransactionById(trans_id);
+		int trans_id2 = trans.getTransferId();
+		if (trans_id2 > 0)
+		{
+			try
+			{
+				RepeatSchedule repeat2 = (RepeatSchedule) this.clone();
+				if (repeat2.write(trans_id2) == -1)
+				{
+					lootDB.endTransaction();
+					return -1;
+				}
+			}
+			catch (CloneNotSupportedException e)
+			{
+				lootDB.endTransaction();
+				return -1;
+			}
+			
+		}
+
+		lootDB.setTransactionSuccessful();
+		lootDB.endTransaction();
+		
+		return this.id;
+	}
+	
+	private int updateRepeat(int trans_id)
+	{
+		if (this.iter == NO_REPEAT)
+		{
+			this.erase();
+			return -1;
+		}
+
+		long end_time = 0;
+		try
+		{
+			end_time = this.end.getTime();
+		}
+		catch (Exception e) { }
+
+		Transaction trans = Transaction.getTransactionById(trans_id);
+		int trans_id2 = trans.getTransferId();
+		int repeat_id2 = -1;
+		if (trans_id2 > 0)
+			repeat_id2 = RepeatSchedule.getRepeatId(trans_id2);
+		
+		String update = "update repeat_pattern set start_date = " + this.start.getTime() +
+						", end_date = " + end_time + ", iterator = " + this.iter +
+						", frequency = " + this.freq + ", custom = " + this.custom +
+						", due = " + this.due.getTime() + " where id = " + this.id;
+		
+		if (repeat_id2 != -1)
+			update += " or id = " + repeat_id2;
+		
+		SQLiteDatabase lootDB = Database.getDatabase();
+		lootDB.beginTransaction();
+		
+		try
+		{
+			lootDB.execSQL(update);
+		}
+		catch (SQLException e)
+		{
+			lootDB.endTransaction();
+			return -1;
+		}
+		
+		// instead of trying to update the row, delete it and copy over the updated transaction
+		if (!this.eraseTransactionFromRepeatTable(true))
+		{
+			lootDB.endTransaction();
+		}
+		
+		if (trans_id2 != -1)
+		{
+			RepeatSchedule repeat2 = RepeatSchedule.getSchedule(repeat_id2);
+			if (!this.writeTransactionToRepeatTable(trans_id) ||
+				!repeat2.writeTransactionToRepeatTable(trans_id2))
+			{
+				lootDB.endTransaction();
+				return -1;
+			}
+		}
+		else
+		{
+			if (!this.writeTransactionToRepeatTable(trans_id))
+			{
+				lootDB.endTransaction();
+				return -1;
+			}
+		}
+		
+		lootDB.setTransactionSuccessful();
+		lootDB.endTransaction();
+		return this.id;
 	}
 	
 	public boolean erase()
@@ -64,11 +220,6 @@ public class RepeatSchedule
 	}
 	
 	private boolean erasePattern(boolean eraseTransfers)
-	{
-		return false;
-	}
-	
-	private boolean eraseTransaction(boolean eraseTransfers)
 	{
 		return false;
 	}
@@ -111,11 +262,6 @@ public class RepeatSchedule
 	public static int[] getDueRepeatIds(Date date)
 	{
 		return null;
-	}
-	
-	public int getTransferId()
-	{
-		return -1;
 	}
 	
 	public static int[] processDueRepetitions(Date date)
@@ -184,8 +330,147 @@ public class RepeatSchedule
 		return true;
 	}
 	
+	public boolean eraseTransactionFromRepeatTable(boolean delete_transfers)
+	{
+		Transaction trans = Transaction.getTransactionById(this.getTransactionId());
+		int repeat_id2 = -1;
+		if (delete_transfers)
+		{
+			int trans_id2 = trans.getTransferId();
+			if (trans_id2 != -1)
+				repeat_id2 = RepeatSchedule.getRepeatId(trans_id2);
+		}
+		
+		String del = "delete from repeat_transactions where repeat_id = " + this.id;
+		if (repeat_id2 != -1)
+			del += " or repeat_id = " + repeat_id2;
+		
+		SQLiteDatabase lootDB = Database.getDatabase();
+		lootDB.beginTransaction();
+		
+		try
+		{
+			lootDB.execSQL(del);
+			lootDB.setTransactionSuccessful();
+		}
+		catch (SQLException e)
+		{
+			return false;
+		}
+		finally
+		{
+			lootDB.endTransaction();
+		}
+		
+		return true;
+	}
+
 	public Date calculateDueDate()
 	{
-		return null;
+		if (this.freq == 0 || this.iter == NO_REPEAT || (this.start.after(this.end) && this.end != null))
+			return null;
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(this.start);
+
+		if ( this.iter == DAILY )
+		{
+			cal.add(Calendar.DAY_OF_MONTH, this.freq);
+		}
+		else if ( this.iter == WEEKLY )
+		{
+			if (this.custom == -1)
+			{
+				// standard weekly repetition
+				cal.add(Calendar.DAY_OF_MONTH, this.freq * 7);
+			}
+			else
+			{
+				// go to next day in the pattern
+				int start_day = cal.get(Calendar.DAY_OF_WEEK);
+				int first_day = cal.getFirstDayOfWeek();
+				int weekday;
+				boolean keep_going = true;
+				
+				while (keep_going)
+				{
+					cal.add(Calendar.DAY_OF_MONTH, 1);
+					weekday = cal.get(Calendar.DAY_OF_WEEK);
+					
+					if (weekday == start_day)
+					{
+						keep_going = false;
+						break;
+					}
+					
+					// if we reached the end of the week, add this.freq - 1 weeks
+					if (weekday == first_day)
+						cal.add(Calendar.DAY_OF_MONTH, 7 * (this.freq - 1));
+					
+					switch (weekday)
+					{
+						case Calendar.MONDAY:
+							if ((this.custom & RepeatSchedule.MONDAY) > 0)
+								keep_going = false;
+							break;
+							
+						case Calendar.TUESDAY:
+							if ((this.custom & RepeatSchedule.TUESDAY) > 0)
+								keep_going = false;
+							break;
+							
+						case Calendar.WEDNESDAY:
+							if ((this.custom & RepeatSchedule.WEDNESDAY) > 0)
+								keep_going = false;
+							break;
+							
+						case Calendar.THURSDAY:
+							if ((this.custom & RepeatSchedule.THURSDAY) > 0)
+								keep_going = false;
+							break;
+							
+						case Calendar.FRIDAY:
+							if ((this.custom & RepeatSchedule.FRIDAY) > 0)
+								keep_going = false;
+							break;
+							
+						case Calendar.SATURDAY:
+							if ((this.custom & RepeatSchedule.SATURDAY) > 0)
+								keep_going = false;
+							break;
+							
+						case Calendar.SUNDAY:
+							if ((this.custom & RepeatSchedule.SUNDAY) > 0)
+								keep_going = false;
+							break;
+						
+						default:
+							keep_going = false;
+							break;
+					}
+				}
+			}
+		}
+		else if (this.iter == MONTHLY)
+		{
+			if (this.custom == DATE)
+			{
+				cal.add(Calendar.MONTH, this.freq);
+			}
+			else if (this.custom == DAY)
+			{
+				int day_of_week = cal.get(Calendar.DAY_OF_WEEK);
+				int day_of_week_in_month = cal.get(Calendar.DAY_OF_WEEK_IN_MONTH);
+				cal.add(Calendar.MONTH, this.freq);
+				cal.set(Calendar.DAY_OF_WEEK, day_of_week);
+				cal.set(Calendar.DAY_OF_WEEK_IN_MONTH, day_of_week_in_month);
+			}
+		}
+		else if (this.iter == YEARLY)
+		{
+			cal.add(Calendar.YEAR, this.freq);
+		}
+		
+		return cal.getTime();
 	}
 }
